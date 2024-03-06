98c6368cf7f0aa4418ee60b1f30dd877bd974094
Andre <andrebomalves@msn.com>
2024-03-06 00:10:25 -0300

    cria classe de pagamento

--- Infnet.Ecommerce.Pagamento.Dominio/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Infnet.Ecommerce.Pagamento.Dominio
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Infnet.Ecommerce.Pagamento.Dominio/Entidades/Pagamento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infnet.Ecommerce.Pagamento.Dominio.Entidades
+{
+    public class Pagamento
+    {
+        public int PagamentoId { get; set; }
+        public int CestaId { get; set; }
+        public decimal ValorTotal { get; set; }
+        public DateTime DataPagamento { get; set; }
+        public StatusPagamento Status { get; set; }
+  
+    }
+
+    public enum StatusPagamento
+    {
+        Pendente,
+        Aprovado,
+        Rejeitado
+    }
+}
